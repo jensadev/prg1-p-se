e1f325b1d1fb07d7afd8583b01420beb7dff07e9
jensadev <jens.andreasson@ntig.se>
2025-11-04 13:10:40 +0100

    Initial implementation of the bag management program with options to view, save, and exit.

---
+++ app.py
@@ -0,0 +1,17 @@
+run = True
+bag = []
+print("Välkommen till påsen 🎒")
+while run:
+    print("Visa innehållet i påsen [V]")
+    print("Spara i påsen [S]")
+    print("Avsluta programmet [Q]")
+    choice = input("Välj: ")
+    if choice.lower() == "v":
+        for thing in bag:
+            print(thing)
+    elif choice.lower() == "s":
+        bag.append(input("Skriv vad du vill spara: "))
+    elif choice.lower() == "q":
+        run = False
+    else:
+        print("Felaktigt kommando, försök igen.")
